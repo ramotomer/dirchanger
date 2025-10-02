804dc4764934db4eceba0aac6a1802bde248e02f
ramotomer <ramotomer@gmail.com>
2025-10-02 17:14:55 +0300

    Added support for multiple file-types in search. To enable adding the open-item action that opens both files and directories whichever it finds

--- dirchanger.py
+++ dirchanger.py
@@ -10,11 +10,13 @@ from exceptions import NoMatchFoundForSpecifier, MultipleNatchesFoundForSpecifie
 from utils import user_friendly_errors
 
 FileCondition_T = Callable[[str, str], bool]
+ActionCallback_T = Callable[[Path], None]
 Specifier_T = str
 
-
+BASE_PATH = Path(r"C:\ramotomer")
 FILE_PATTERNS_TO_IGNORE = [
     r".*~",
+    r"#.*#",
 ]
 
 
@@ -27,14 +29,33 @@ class ActionType(Enum):
     OPEN_FILE_EXPLORER = "open_file_explorer"
     OPEN_FILE          = "open_file"
     OPEN_CMD           = "open_cmd"
+    OPEN_ITEM          = "open_item"
+
+    @property
+    def should_open_file(self) -> bool:
+        return self in [ActionType.OPEN_ITEM, ActionType.OPEN_FILE]
+
+    @property
+    def should_open_dir(self) -> bool:
+        return self in [ActionType.OPEN_ITEM, ActionType.OPEN_FILE_EXPLORER]
 
 
 class DirectoryListing(NamedTuple):
     directory: List[str]
     file:      List[str]
 
-    def of_type(self, item_type: ItemType):
-        return getattr(self, item_type.value)
+    def filter_types(self, item_types: List[ItemType]):
+        items = []
+        assert len(item_types) == len(set(item_types)), f"Duplicates not expected in: {item_types}"
+        for item_type in item_types:
+            if item_type == ItemType.FILE:
+                items.extend(self.file)
+                continue
+            if item_type == ItemType.DIRECTORY:
+                items.extend(self.directory)
+                continue
+            raise ValueError(f"Unsupported item type {item_type}")
+        return items
 
 
 def file_condition__prefix(filename: str, specifier: str) -> bool:
@@ -100,14 +121,17 @@ FILE_CONDITIONS: List[FileCondition_T] = [
 ]
 
 
-def raise_could_not_locate_path(matched_files: List[str], searched_item_type: ItemType, searched_path: Path, specifier: Specifier_T) -> None:
+def raise_could_not_locate_path(matched_files: List[str], searched_item_types: List[ItemType], searched_path: Path, specifier: Specifier_T) -> None:
+    types_str = ','.join([item_type.value for item_type in searched_item_types[:-1]])
+    types_str += ' or '.join(([types_str] if types_str else []) + [searched_item_types[-1].value])
+
     if not matched_files:
-        raise NoMatchFoundForSpecifier(f"No {searched_item_type.value} in {str(searched_path)!r} matched specifier {specifier!r}!")
-    raise MultipleNatchesFoundForSpecifier(f"More than one {searched_item_type.value} in {str(searched_path)!r} matched specifier {specifier!r}!\n{matched_files}")
+        raise NoMatchFoundForSpecifier(f"No {types_str} in {str(searched_path)!r} matched specifier {specifier!r}!")
+    raise MultipleNatchesFoundForSpecifier(f"More than one {types_str} in {str(searched_path)!r} matched specifier {specifier!r}!\n{matched_files}")
 
 
-def choose_file_by_specifier(current_path: Path, next_specifier: Specifier_T, current_item_type: ItemType) -> Path:
-    items = get_directory_listing(current_path).of_type(current_item_type)
+def choose_file_by_specifier(current_path: Path, next_specifier: Specifier_T, desired_item_types: List[ItemType]) -> Path:
+    items = get_directory_listing(current_path).filter_types(desired_item_types)
 
     matched_files = []
     for condition in FILE_CONDITIONS:
@@ -116,50 +140,66 @@ def choose_file_by_specifier(current_path: Path, next_specifier: Specifier_T, cu
         if len(matched_files) == 1:
             return current_path / Path(matched_files[0])
         continue
+    raise_could_not_locate_path(matched_files, desired_item_types, current_path, next_specifier)
 
-    raise_could_not_locate_path(matched_files, current_item_type, current_path, next_specifier)
 
-
-def locate_complete_path(base_path: Path, target_item_type: ItemType, specifiers: List[str]) -> Path:
+def locate_complete_path(base_path: Path, target_item_types: List[ItemType], specifiers: List[str]) -> Path:
     current_path = base_path
 
     while specifiers:
-        current_item_type = target_item_type if len(specifiers) == 1 else ItemType.DIRECTORY
+        is_last_iteration = (len(specifiers) == 1)
+        current_item_type = target_item_types if is_last_iteration else [ItemType.DIRECTORY]
 
         current_path = choose_file_by_specifier(current_path, specifiers[0], current_item_type)
         specifiers = specifiers[1:]
-
     return current_path
 
 
-def open_in_file_explorer(path: Path) -> None:
+def action_callback__open_in_file_explorer(path: Path) -> None:
     Popen(f"explorer \"{path!s}\"")
 
 
-def open_cmd_at(path: Path) -> None:
+def action_callback__open_cmd_at(path: Path) -> None:
     Popen(f"cmd /K cd \"{path!s}\"")
+
+
+def get_item_types(action_type: ActionType) -> List[ItemType]:
+    if action_type == ActionType.OPEN_FILE_EXPLORER:
+        return [ItemType.DIRECTORY]
+    if action_type == ActionType.OPEN_FILE:
+        return [ItemType.FILE]
+    if action_type == ActionType.OPEN_CMD:
+        return [ItemType.DIRECTORY]
+    if action_type == ActionType.OPEN_ITEM:
+        return [ItemType.DIRECTORY, ItemType.FILE]
+    raise ValueError(f"Unsupported action of type {action_type}")
+
+
+def get_action(action_type: ActionType) -> ActionCallback_T:
+    if action_type in [
+            ActionType.OPEN_FILE_EXPLORER,
+            ActionType.OPEN_FILE,
+            ActionType.OPEN_ITEM,
+        ]:
+        return action_callback__open_in_file_explorer
+
+    if action_type == ActionType.OPEN_CMD:
+        return action_callback__open_cmd_at
+
+    raise ValueError(f"Invalid action {action_type}")
+
+
+def main():
+    action_type = ActionType(sys.argv[1])
+    user_specifiers = sys.argv[2:]
+
+    action = get_action(action_type)
+    item_types = get_item_types(action_type)
+
+    path = locate_complete_path(BASE_PATH, item_types, user_specifiers)
+    action(path)
 
 
 if __name__ == '__main__':
     with user_friendly_errors():
-        BASE_PATH = Path(r"C:\ramotomer")
-        action_type = ActionType(sys.argv[1])
-        user_specifiers = sys.argv[2:]
-
-        if action_type == ActionType.OPEN_FILE_EXPLORER:
-            action = open_in_file_explorer
-            item_type = ItemType.DIRECTORY
-
-        elif action_type == ActionType.OPEN_FILE:
-            action = open_in_file_explorer
-            item_type = ItemType.FILE
-
-        elif action_type == ActionType.OPEN_CMD:
-            action = open_cmd_at
-            item_type = ItemType.DIRECTORY
-
-        else:
-            raise ValueError(f"Invalid action {action_type}")
-
-        path = locate_complete_path(BASE_PATH, item_type, user_specifiers)
-        action(path)
+        main()
